c2958acbd26918880abb118178ee3504a09c37c9
auliamasadah04 <auliamasadah@gmail.com>
2022-03-07 11:12:55 +0700

    Tugas

---
+++ src/Vehicle.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author aulia
+ */
+public abstract class Vehicle {
+    
+    public void function(){
+        System.out.println("Tools Transportation");
+    }
+    public void fuel(){
+        System.out.println("fuels");
+    }
+    public abstract void walk();
+}
